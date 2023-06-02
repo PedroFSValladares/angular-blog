bd91cf051477399569c9353f6e7913365bf53b07
Pedro Valladares <pedrohvalladares@hotmail.com>
2023-06-02 11:11:08 -0300

    Created article card struct

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import News from '../../../assets/data/news.json';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit{
+  title:string = News.title
+  summary:string = this.makeSummary()
+  topic:string = News.topic
+  views:number = 2000
+  publishedAt!: Date;
+  imageUrl:string = 'News.imageUrl'
 
+  ngOnInit(): void {
+      
+  }
+  makeSummary():string{
+    let fullText = News.text[0].text.split(' ')
+    return fullText.slice(0, 20).join(' ').concat('...')
+  }
 }
